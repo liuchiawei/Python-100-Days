fc2c0eedf96ae4e0978c8a1edd0996250640259d
liuchiawei <doublecheap@gmail.com>
2026-03-17 14:13:22 +0900

    Day2, Day3

---
+++ Day01-20/02-helloworld.py
@@ -0,0 +1,6 @@
+print("Hello, World!") #comment
+print("Goodbyr, World!")
+"""
+''' is a multi-line comment
+this is a multi-line comment
+"""
---
+++ Day01-20/03-variables.py
@@ -0,0 +1,56 @@
+# Day 3 Variables 變數
+# 整型 int
+print('整型(int)')
+print('--------------------------------')
+print(0b100) #二進位
+print(0o100) #八進位
+print(100) #十進位
+print(0x100) #十六進位
+print('--------------------------------')
+# 浮點型 float
+print('浮點型(float)')
+print('--------------------------------')
+print(123.456) #數學寫法
+print(1.23456e2) #科學記數法
+print('--------------------------------')
+
+# 字串型 str
+print('字串(str)')
+print('--------------------------------')
+print('hello, world')
+print("hello, world")
+print('''hello, world''')
+print("""hello, world""")
+print('--------------------------------')
+# 布爾型 bool
+print('布爾型(bool)')
+print('--------------------------------')
+print(True)
+print(False)
+print('--------------------------------')
+# 空值型 None
+print('空值型(None)')
+print('--------------------------------')
+print(None)
+
+#變量的使用
+a = 45
+b = 12
+print(a, b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+c = 'hello, world'
+d = True
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(None, type(None))
+# int():將一個數值或字符串轉換成整數
+# float():將一個數值或字符串轉換成浮點數
+# str():將一個數值或字符串轉換成字符串
+# bool():將一個數值或字符串轉換成布爾值
+# chr():將一個整數轉換成對應的字符
+# ord():將一個字符轉換成對應的整數
